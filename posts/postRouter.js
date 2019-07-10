1da0de2cc368fc731df06f78eede6e0a9e555fa6
Josemaria <josemarianriagu25@gmail.com>
2019-07-10 22:54:34 +0800

    implement validatePostId middleware implement GET by id api

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -14,8 +14,12 @@ router.get('/', async (req, res) => {
    }
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validatePostId, (req, res) => {
+   try {
+      res.status(200).json(req.post);
+   } catch (error) {
+      res.status(500).json({ message: 'Oops, something went wrong' })
+   }
 });
 
 router.delete('/:id', (req, res) => {
@@ -28,8 +32,21 @@ router.put('/:id', (req, res) => {
 
 // custom middleware
 
-function validatePostId(req, res, next) {
-
+async function validatePostId(req, res, next) {
+   let { id } = req.params;
+   id = Number(id);
+   if (Number.isInteger(id)) {
+      req.valid = true;
+      const post = await Posts.getById(id)
+      if (post) {
+         req.post = post;
+         next();
+      } else {
+         res.status(404).json({ message: 'the post with that id has gone to Mars!' });
+      }
+   } else {
+      res.set('X-Nasty', 'Nasty ID').status(400).json({ message: "that does not look like an id!!" });
+   }
 };
 
 module.exports = router;
