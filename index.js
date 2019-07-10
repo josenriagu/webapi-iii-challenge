47155664a38c1b8e7d4da2fb0f736f5c96127f12
Josemaria <josemarianriagu25@gmail.com>
2019-07-10 19:35:28 +0800

    define index file

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code away!
+const port = 5000
+const server = require('./server.js');
+
+server.listen(port, () => {
+   console.log(`\n* Awesome!! *\n* Server Running on http://localhost:${5000} *\n`);
+});
